fe18880bc21234312370fc8d6da00c34dc0a5aaa
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-19 21:12:57 +0100

    Add gravity

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 const SPEED = 750
+const GRAVITY = 1000
+const UP = Vector2(0, -1)
 
 var motion = Vector2()
 
@@ -8,20 +10,32 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+    fall(delta)
+    run()
+    jump()
+    move_and_slide(motion, UP)
+
+func run():
     motion.x = 0
+    $AnimatedSprite.flip_h = false
     if Input.is_action_pressed("ui_right"):
         motion.x += SPEED
         $AnimatedSprite.play('run')
-        $AnimatedSprite.flip_h = false
     if Input.is_action_pressed("ui_left"):
         motion.x -= SPEED
         $AnimatedSprite.play('run')
         $AnimatedSprite.flip_h = true
     if motion.x == 0:
         $AnimatedSprite.play('idle')
-        $AnimatedSprite.flip_h = false
     
     if Input.is_action_pressed("ui_up"):
         $AnimatedSprite.play('jump')
-        $AnimatedSprite.flip_h = false
-    move_and_slide(motion)
+
+func fall(delta):
+    if is_on_floor():
+        motion.y = 0
+    else:
+        motion.y += GRAVITY * delta
+
+func jump():
+    pass
