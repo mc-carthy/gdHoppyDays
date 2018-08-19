162de3403861d6d35ca660734e65921ca98911ea
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-08-19 20:33:39 +0100

    Add animation

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,8 +11,17 @@ func _physics_process(delta):
     motion.x = 0
     if Input.is_action_pressed("ui_right"):
         motion.x += SPEED
-    if Input.is_action_pressed("ui_left"):
-        motion.x -= SPEED
+        $AnimatedSprite.play('run')
+        $AnimatedSprite.flip_h = false
+        if Input.is_action_pressed("ui_left"):
+            motion.x -= SPEED
+            $AnimatedSprite.play('run')
+            $AnimatedSprite.flip_h = true
+        if motion.x == 0:
+            $AnimatedSprite.play('idle')
+            $AnimatedSprite.flip_h = false
+    
     if Input.is_action_pressed("ui_up"):
-        pass
+        $AnimatedSprite.play('jump')
+        $AnimatedSprite.flip_h = false
     move_and_slide(motion)
