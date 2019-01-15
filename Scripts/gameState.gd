adcc43a13ed55e63ba39b9f98b3f22aa463c3c33
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-01-15 22:22:04 +0000

    Add life count GUI updating

--- Scripts/gameState.gd
+++ Scripts/gameState.gd
@@ -6,10 +6,15 @@ var lives
 func _ready():
     global.gameState = self
     lives = startingLives
+    update_gui()
+
+func update_gui():
+    global.gui.update_gui(lives)
 
 func hurt():
     global.player.hurt()
     lives -= 1
+    update_gui()
     if lives < 0:
         end_game()
 
--- Scripts/gui.gd
+++ Scripts/gui.gd
@@ -2,3 +2,6 @@ extends CanvasLayer
 
 func _ready():
     global.gui = self
+
+func update_gui(lives):
+    $banner/HBoxContainer/lifeCount.text = str(lives)
