34950609c7da1f367bdbf0b31a7419542b827d52
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-01-16 21:27:51 +0000

    Use onready variables to reference nodes

--- Scripts/gameState.gd
+++ Scripts/gameState.gd
@@ -6,29 +6,31 @@ export var coins_for_extra_life = 20
 var lives
 var coins = 0
 
+onready var GUI = global.gui
+
 func _ready():
     global.gameState = self
     lives = startingLives
     update_gui()
 
 func update_gui():
-    global.gui.update_gui(lives, coins)
+    GUI.update_gui(lives, coins)
 
 func hurt():
     global.player.hurt()
-    global.gui.hurt()
+    GUI.hurt()
     lives -= 1
     update_gui()
     if lives < 0:
         end_game()
 
 func coin_up():
-    global.gui.coin_up()
+    GUI.coin_up()
     coins += 1
     update_gui()
     if coins % coins_for_extra_life == 0:
         lives += 1
-        global.gui.life_up()
+        GUI.life_up()
         update_gui()
 
 func end_game():
