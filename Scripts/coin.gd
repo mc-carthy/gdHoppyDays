e268004d0aa0f75099d36cf424039908445477fd
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-01-19 22:04:47 +0000

    Fix coin pickup bug

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,10 +1,13 @@
 extends AnimatedSprite
 
+var taken = false
 
 func _on_body_entered(body):
-    global.gameState.coin_up()
-    $AnimationPlayer.play('die')
-    global.coin_sfx.play()
+    if not taken:
+        taken = true
+        global.gameState.coin_up()
+        $AnimationPlayer.play('die')
+        global.coin_sfx.play()
 
 func die():
     queue_free()
